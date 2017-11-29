dbcfefbeb7a34a18210dd2c63f1494f01f4bd0a7
manzoli2122 <manzoli2122@gmail.com>
2017-11-29 09:16:12 -0200

    initial

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AALSetupTables extends Migration
+class AalSetupPerfilPermissaoTables extends Migration
 {
    
 
